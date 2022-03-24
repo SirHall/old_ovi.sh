ce3e347351565b61f6dced4ecb44af409461c4a2
SirHall <sirhallstein@gmail.com>
2022-03-24 20:30:36 +1000

    Get simple fullscreen quad shader functional

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
-// @ts-ignore
-import { BloomEffect, EffectComposer, EffectPass, RenderPass, ShaderPass, } from "../node_modules/postprocessing/build/postprocessing.mjs";
 
 const time = new THREE.Clock();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.01, 1.0);
+camera.position.z = 0.5;
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const tau = 6.28318530717958647692528676655900577;
 
@@ -17,56 +17,58 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.PlaneGeometry(2.0, 2.0);
 
-camera.position.z = 5;
+
+const material = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 0.0 },
+        resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) }
+    },
+
+    vertexShader: `
+        varying vec2 vUv;
+
+        void main()
+        {
+            vUv = uv;
+            gl_Position = vec4( position, 1.0 );
+        }
+    `,
+
+
+    fragmentShader: `
+        varying vec2 vUv;
+
+        uniform float time;
+
+        void main()	{
+
+            gl_FragColor = vec4(1.0);
+
+        }
+
+    `,
+});
+
+
+const quad = new THREE.Mesh(geometry, material);
+scene.add(quad);
 
 // const fontLoader = new THREE.FontLoader();
 
 
-/**
- * Full-screen textured quad shader
- */
-const pass = new ShaderPass(new THREE.ShaderMaterial({
 
-    defines: { LABEL: "value" },
-    uniforms: {
-        tDiffuse: new THREE.Uniform(null),
-        opacity: new THREE.Uniform(1.0)
-    },
-
-    vertexShader: /* glsl */`
-        varying vec2 vUv;
-        void main() {
-            vUv = uv;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-        }`,
-
-    fragmentShader: /* glsl */`
-        uniform float opacity;
-        uniform sampler2D tDiffuse;
-        varying vec2 vUv;
-        void main() {
-            vec4 texel = texture2D( tDiffuse, vUv );
-            gl_FragColor = opacity * texel + vec4(1.0, 0.0, 0.0, 1.0);
-        }`
-
-
-}), "tDiffuse");
-
-const composer = new EffectComposer(renderer);
-composer.addPass(pass);
 
 function animate() {
     requestAnimationFrame(animate);
 
     const dt = time.getDelta();
 
-    cube.rotation.x += 0.60 * dt;
-    cube.rotation.y += 0.60 * dt;
+    // cube.rotation.x += 0.60 * dt;
+    // cube.rotation.y += 0.60 * dt;
+
+    material.uniforms.time.value = performance.now() / 1000.0;
 
     renderer.render(scene, camera);
 };
